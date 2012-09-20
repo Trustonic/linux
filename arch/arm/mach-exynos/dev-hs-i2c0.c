3fb6625e7825c0b72dbcac8d617613eb2ed9a25e
Hyeonkook Kim <hk619.kim@samsung.com>
2012-09-20 09:30:17 +0900

    ARM: EXYNOS: enable interrupt mode for HS-I2C
    Change-Id: I64c94c9474f92d07f43678acb0a2c4043bea152b
    Signed-off-by: Hyeonkook Kim <hk619.kim@samsung.com>

--- arch/arm/mach-exynos/dev-hs-i2c0.c
+++ arch/arm/mach-exynos/dev-hs-i2c0.c
@@ -34,8 +34,8 @@ struct platform_device exynos5_device_hs_i2c0 = {
 
 struct exynos5_platform_i2c default_hs_i2c_data __initdata = {
 	.bus_number = 0,
-	.operation_mode = 0,
-	.speed_mode = 0,
+	.operation_mode = HSI2C_INTERRUPT,
+	.speed_mode = HSI2C_FAST_SPD,
 	.fast_speed = 400000,
 	.high_speed = 2500000,
 	.cfg_gpio = NULL,
