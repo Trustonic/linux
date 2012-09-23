816e444e59ccc8f7ef7c2ebecf74b59454331a68
Won Jongbin <jongbin.won@samsung.com>
2012-09-23 15:14:11 +0900

    ARM: EXYNOS: Updated the definitions related to Secmem driver
    Change-Id: I2125368e5373b4b1300642a3fdd50ca92a1484c8
    Signed-off-by: Won Jongbin <jongbin.won@samsung.com>

--- arch/arm/mach-exynos/include/mach/secmem.h
+++ arch/arm/mach-exynos/include/mach/secmem.h
@@ -55,8 +55,10 @@ void secmem_crypto_deregister(void);
 #define SECMEM_IOC_GET_DRM_ONOFF	_IOWR('S', 3, int)
 #define SECMEM_IOC_GET_CRYPTO_LOCK	_IOR('S', 4, int)
 #define SECMEM_IOC_RELEASE_CRYPTO_LOCK	_IOR('S', 5, int)
+#define SECMEM_IOC_GET_ADDR             _IOWR('S', 6, int)
+#define SECMEM_IOC_RELEASE_ADDR         _IOWR('S', 7, int)
 #if defined(CONFIG_SOC_EXYNOS5250)
-#define SECMEM_IOC_GET_FD_PHYS_ADDR    _IOWR('S', 8, struct secfd_info)
+#define SECMEM_IOC_GET_FD_PHYS_ADDR    _IOWR('S', 8, int)
 #endif
 
 #endif /* __ASM_ARCH_SECMEM_H */
