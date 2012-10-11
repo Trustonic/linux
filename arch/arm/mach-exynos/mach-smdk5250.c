3a7f64b080466c5b3ab3c4d8f8726ba623315d64
Huisung Kang <hs1218.kang@samsung.com>
2012-10-11 14:11:04 +0900

    ARM: exynos: smdk5250: change backlight pwm to 1KHz.
    Change-Id: Iaa8da6d6cbfe2799daeeb2915e751f89e69c0c68
    Signed-off-by: Huisung Kang <hs1218.kang@samsung.com>

--- arch/arm/mach-exynos/mach-smdk5250.c
+++ arch/arm/mach-exynos/mach-smdk5250.c
@@ -1605,7 +1605,7 @@ static struct samsung_bl_gpio_info smdk5250_bl_gpio_info = {
 
 static struct platform_pwm_backlight_data smdk5250_bl_data = {
 	.pwm_id		= 0,
-	.pwm_period_ns	= 30000,
+	.pwm_period_ns	= 1000000,
 	.dft_brightness	= 102,
 };
 
