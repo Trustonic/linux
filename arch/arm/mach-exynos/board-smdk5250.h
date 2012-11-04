f8913b73df326e5f31a2eec681368fc0f18ddc69
Huisung Kang <hs1218.kang@samsung.com>
2012-11-04 13:06:42 +0900

    ARM: exynos: smdk5250: Move display codes to board-smdk5250-display.c
    Change-Id: I181b22baa6cf329da4781c25da3df3df40026eae
    Signed-off-by: Huisung Kang <hs1218.kang@samsung.com>

--- arch/arm/mach-exynos/board-smdk5250.h
+++ arch/arm/mach-exynos/board-smdk5250.h
@@ -45,5 +45,6 @@ void exynos5_smdk5250_input_init(void);
 void exynos5_smdk5250_usb_init(void);
 void exynos5_smdk5250_power_init(void);
 void exynos5_smdk5250_spi_init(void);
+void exynos5_smdk5250_display_init(void);
 
 #endif
