6a54c72eb5df5842d5160b90f7137548d9f6d8f2
Huisung Kang <hs1218.kang@samsung.com>
2012-11-04 12:22:58 +0900

    ARM: exynos: smdk5250: Move spi codes to board-smdk5250-spi.c
    Change-Id: Ib7988339b05980cf8f3b9de23cb0fbc7ec7deeb4
    Signed-off-by: Huisung Kang <hs1218.kang@samsung.com>

--- arch/arm/mach-exynos/board-smdk5250.h
+++ arch/arm/mach-exynos/board-smdk5250.h
@@ -44,5 +44,6 @@ void exynos5_smdk5250_audio_init(void);
 void exynos5_smdk5250_input_init(void);
 void exynos5_smdk5250_usb_init(void);
 void exynos5_smdk5250_power_init(void);
+void exynos5_smdk5250_spi_init(void);
 
 #endif
