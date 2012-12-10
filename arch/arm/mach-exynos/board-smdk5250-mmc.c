6d77e1ea830b49fac8c9b3d2e3a13920ff9ea5c8
Huisung Kang <hs1218.kang@samsung.com>
2012-12-11 08:31:05 +0900

    ARM: exynos: smdk5250: Enable DW_MMC_QUIRK_HW_RESET_PW
    This enables resetting the emmc chip when trancaction errors are detected.
    
    Change-Id: Ic61143e4e4705173d5e142a6b545b48a74a80d30
    Signed-off-by: Huisung Kang <hs1218.kang@samsung.com>

--- arch/arm/mach-exynos/board-smdk5250-mmc.c
+++ arch/arm/mach-exynos/board-smdk5250-mmc.c
@@ -61,6 +61,7 @@ static struct dw_mci_board exynos_dwmci0_pdata __initdata = {
 	.num_slots		= 1,
 	.quirks			= DW_MCI_QUIRK_BROKEN_CARD_DETECTION |
 				  DW_MCI_QUIRK_HIGHSPEED |
+				  DW_MMC_QUIRK_HW_RESET_PW |
 				  DW_MCI_QUIRK_NO_DETECT_EBIT,
 	.bus_hz			= 200 * 1000 * 1000,
 	.caps			= MMC_CAP_UHS_DDR50 | MMC_CAP_1_8V_DDR |
