52d0fcd61b44fd35aea5d344179cf6b57a7ff1db
Anton Tikhomirov <av.tikhomirov@samsung.com>
2012-12-10 16:04:27 +0900

    ARM: SAMSUNG: Add PHY tuning callback declaration
    Change-Id: Ibd06da8815aeee71d43015a9cc999cd5d287e97f
    Signed-off-by: Anton Tikhomirov <av.tikhomirov@samsung.com>

--- arch/arm/plat-samsung/include/plat/usb-phy.h
+++ arch/arm/plat-samsung/include/plat/usb-phy.h
@@ -19,6 +19,7 @@ enum s5p_usb_phy_type {
 
 extern int s5p_usb_phy_init(struct platform_device *pdev, int type);
 extern int s5p_usb_phy_exit(struct platform_device *pdev, int type);
+extern int s5p_usb_phy_tune(struct platform_device *pdev, int type);
 extern int s5p_usb_phy_suspend(struct platform_device *pdev, int type);
 extern int s5p_usb_phy_resume(struct platform_device *pdev, int type);
 extern int exynos5_usb_phy_crport_ctrl(struct platform_device *pdev,
